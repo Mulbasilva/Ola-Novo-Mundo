2ac5c7f9af9a4a319b0c29c6136dfd3faa9c04ba
Mulbasilva <dideus24@gmail.com>
2022-11-20 13:03:49 -0300

    modificacoes no formulario (validacao), algumas alteracoes feitas

--- js/javascript.js
+++ js/javascript.js
@@ -3,14 +3,51 @@ const campos = document.querySelectorAll('.required');
 const spans = document.querySelectorAll('.orientacao');
 const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-// function seError(index)
+form.addEventListener('submit', (Event) => {
+    Event.preventDefault();
+    nameValidate();
+    emailValidate();
+}); 
+
+function setError(index){
+    campos[index].style.border = '2px solid #e63636';
+    spans[index].style.display = 'block';
+}
+
+function removeError(index){
+campos[index].style.border = '';
+spans[index].style.display = 'none';
+}
+
+
 function nameValidate(){
-    if(campos[0].value.length < 3)
+   if(campos[0].value.length < 3)
     { 
-    console.log('Este campo deve conter no mínino 3 letras');
-     }
+        setError(0);
+    }
      else
      {
-        console.log('Nome validado!');
+        removeError(0);
      }
+}
+
+function emailValidate(){
+    if(!emailRegex.test(campos[1].value))
+    {
+        setError(1);
+    }
+    else{
+        removeError(1);    
+    }
+}
+
+function texareaValidate(){
+    if(campos[2].value.length < 15)
+    { 
+        setError(2);
+    }
+    else
+    {
+        removeError(2); 
+    }
 }
